30616bc0335ecf21f1042353127f7d418d72c341
Hao Li <hli197@my.bcit.ca>
2022-04-01 21:56:58 -0700

    added new event and new function  which cloned from original div inside to copy-container.

---
+++ src/app.js
@@ -0,0 +1,23 @@
+// clone
+
+const copy = document.querySelector('.copy-container')
+const cloned = document.querySelectorAll('.original-container div')
+
+function addClicks(event) {
+    copy.insertAdjacentHTML('afterbegin', event)
+}
+
+cloned.forEach(element => element.addEventListener('click', (evt) => {
+    addClicks(evt.currentTarget.outerHTML)
+}))
+
+// delete
+
+const Delete = document.querySelector('button')
+
+function deleteClicks() {
+    while (copy.lastChild) {
+        copy.removeChild(copy.lastChild)}
+}
+
+Delete.addEventListener('click', deleteClicks)
